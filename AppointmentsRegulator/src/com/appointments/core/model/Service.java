4f2c9f0f2cd76c808e088193b2f601f6eb637fb1
abdallahmoustafa <abdallahmoustafa@192.168.1.103>
2020-10-09 15:22:08 +0200

    Add service description

--- AppointmentsRegulator/src/com/appointments/core/model/Service.java
+++ AppointmentsRegulator/src/com/appointments/core/model/Service.java
@@ -2,4 +2,21 @@ package com.appointments.core.model;
 
 public class Service {
 
+	private String serviceName;
+	private String description;
+	
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
